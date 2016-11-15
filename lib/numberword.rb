61971dc486604f7a755715780eeaf0c967dab7a4
Kenneth Black and getro Naissance <kenneth+getro@>
2016-11-15 11:59:20 -0800

    add logic um to solve thru 999

--- lib/numberword.rb
+++ lib/numberword.rb
@@ -1,5 +1,78 @@
 class Fixnum
   define_method(:numberword) do
+    ones = {
+      1  => "one",
+      2  => "two",
+      3  => "three",
+      4  => "four",
+      5  => "five",
+      6  => "six",
+      7  => "seven",
+      8  => "eight",
+      9  => "nine",
+      10  => "ten",
+      11  => "eleven",
+      12  => "twelve",
+      13  => "thirteen",
+      14  => "fourteen",
+      15  => "fifteen",
+      16  => "sixteen",
+      17  => "seventeen",
+      18  => "eighteen",
+      19  => "nineteen"
+    }
+    tens = {
+      2 => "twenty",
+      3 => "thirty",
+      4 => "fourty",
+      5 => "fifty",
+      6 => "sixty",
+      7 => "seventy",
+      8 => "eighty",
+      9 => "ninety"
+    }
+    higher_digits = {
+      4 => "thousand",
+      7 => "million",
+      10 => "billion",
+      13 => "trillion"
+    }
+    # assume 3 digit number. split into threes for higher
+    sentence = []
+    groups = []
+    digits = self.to_s
+    for i in 1..(digits.length/3.0).ceil do
+      if(digits.length > 3)
+        groups.push(digits.slice!(-3, 3).to_i)
+      else
+        groups.push(digits.to_i)
+      end
+    end
+
+    groups.reverse.each do |num|
+      if num >= 100
+        sentence.push(ones.fetch(num/100))
+        sentence.push("hundred")
+        num = num % 100
+      end
+
+      if num >= 20
+        sentence.push(tens.fetch(num/10))
+        num = num % 10
+      end
+      if ones.has_key?(num)
+      	sentence.push(ones.fetch(num))
+      end
+    end
     
+    # num = self.to_s.split("").map(&:to_i)
+    # if num.length == 3
+    #   sentence.push(ones[num[0]])
+    #   sentence.push("hundred")
+    # end
+    # if num.length >= 1 or (num.length == 2 and num[0] == 1)
+    #   sentence.push(ones[num[0]])
+    # end
+    sentence.join(" ")
   end
 end
--- spec/numberword_spec.rb
+++ spec/numberword_spec.rb
@@ -3,7 +3,7 @@ require('numberword')
 
 describe('Fixnum#numberword') do
   it('returns a string') do
-    expect(123.numberword.class).to(eq(String))
+    expect(12.numberword.class).to(eq(String))
   end
 
   it('should return "nine" for the number 9') do
@@ -17,5 +17,15 @@ describe('Fixnum#numberword') do
   it('should return "fifty" for the number 50') do
     expect(50.numberword).to(eq("fifty"))
   end
-  
+
+  it('should return "nintety nine" for the number 99') do
+    expect(99.numberword).to(eq("ninety nine"))
+  end
+  it('should return "one hundred twenty three" for 123') do
+    expect(123.numberword).to(eq("one hundred twenty three"))
+  end
+  it('should return "one thousand twenty two" for the number 1022') do
+    expect(1022.numberword).to(eq("one thousand twenty two"))
+  end
+
 end
